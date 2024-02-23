757e35993134148696c51ea6a44eecc70e031392
danvitmac <daniel.atdr@gmail.com>
2024-02-23 21:03:42 +0000

    feat: new props component card-news

--- src/app/components/card-news/card-news.component.ts
+++ src/app/components/card-news/card-news.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-news',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-news.component.css']
 })
 export class CardNewsComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
 
   constructor() { }
 
